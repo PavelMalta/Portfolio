aacda897751e973ff99acf6415770e21582d28a8
PavelMalta <shamashov87@mail.ru>
2021-04-05 21:58:08 +0300

    add sass in Title.module.scss and Project.module.scss

--- src/common/components/title/Title.tsx
+++ src/common/components/title/Title.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from "./Title.module.css"
+import s from "./Title.module.scss"
 
 type TitlePropsTitle = {
     title: string
@@ -7,10 +7,10 @@ type TitlePropsTitle = {
 
 export function Title (props: TitlePropsTitle) {
     return (
-        <div>
-        <div className={s.title}>
-            <h2>{props.title}</h2>
-        </div>
+        <div className={s.container}>
+            <div className={s.title}>
+                <h2>{props.title}</h2>
+            </div>
             <div className={s.separator}>
                 <span>xxx</span>
             </div>
