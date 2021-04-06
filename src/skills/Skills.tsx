a6e49b834620ae4a0206bb53a295b22aa91a264d
PavelMalta <shamashov87@mail.ru>
2021-04-06 17:25:36 +0300

    add images in Skill component

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,20 +3,34 @@ import s from './Skills.module.css'
 import styleContainer from '../../src/common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
+import htmlImg from "../common/image/skills/HTML.jpg";
+import cssImg from "../common/image/skills/1476977488.jpg";
+import jsImg from "../common/image/skills/JS.jpg";
+import reactImg from "../common/image/skills/maxresdefault.jpg";
+import reduxImg from "../common/image/skills/images.png";
+import typeScriptImg from "../common/image/skills/Typescript.jpg";
 
 export function Skills () {
-    const description = 'An integrated development environment in JavaScript, ' +
-                        'CSS & HTML from JetBrains, developed on the basis of ' +
-                        'the IntelliJ IDEA platform. WebStorm provides auto-completion'
+
+    const html = {backgroundImage: `url(${htmlImg})`}
+    const css = {backgroundImage: `url(${cssImg})`}
+    const js = {backgroundImage: `url(${jsImg})`}
+    const react = {backgroundImage: `url(${reactImg})`}
+    const redux = {backgroundImage: `url(${reduxImg})`}
+    const typeScript = {backgroundImage: `url(${typeScriptImg})`}
+
     return(
         <div className={s.skillsBlock}>
             <hr className={styleContainer.top_dashed}/>
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <Title title={'Skills'}/>
                 <div className={s.skills}>
-                    <Skill title={"HTML"} description={description}/>
-                    <Skill title={"CSS"} description={description}/>
-                    <Skill title={"JS"} description={description}/>
+                    <Skill title={"HTML"} style={html}/>
+                    <Skill title={"CSS"} style={css}/>
+                    <Skill title={"JS"} style={js}/>
+                    <Skill title={"React"} style={react}/>
+                    <Skill title={"Redux"} style={redux}/>
+                    <Skill title={"TypeScript"} style={typeScript}/>
                 </div>
             </div>
             <hr className={styleContainer.bottom_dashed}/>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -3,17 +3,14 @@ import s from './Skill.module.css'
 
 type SkillPropsType = {
     title: string
-    description: string
+    style: object
 }
 
 export function Skill (props: SkillPropsType) {
     return (
         <div className={s.skill}>
-            <div className={s.icon}></div>
+            <div className={s.icon} style={props.style}></div>
             <h3>{props.title}</h3>
-            <span className={s.description}>
-                {props.description}
-            </span>
         </div>
     )
 }
