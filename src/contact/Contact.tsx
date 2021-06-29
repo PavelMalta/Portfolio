fd16193635016dda2acd00c0c70921992b02c590
PavelMalta <shamashov87@mail.ru>
2021-06-29 16:46:08 +0300

    connected formik, added useFormik

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -3,6 +3,7 @@ import s from "./Contact.module.css"
 import {Title} from "../common/components/title/Title";
 import styleContainer from "../common/styles/Container.module.css";
 import axios from "axios";
+import { useFormik } from "formik";
 
 
 export function Contact() {
@@ -14,18 +15,47 @@ export function Contact() {
             })
     },[])
 
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            contacts: '',
+            message: ''
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        },
+    })
+
+
 
     return (
         <div className={s.contactsBlock}>
             <hr className={styleContainer.top_dashed}/>
             <div className={s.contactsContainer}>
                 <Title title={'Contact'}/>
-                <form action="#" className={s.contacts}>
-                    <input type="text" className={s.input} placeholder={"Name"}/>
-                    <input type="text" className={s.input} placeholder={"E-mail"}/>
-                    <textarea className={s.textarea} placeholder={"Your message"}></textarea>
+                <form action="#" className={s.contacts} onSubmit={formik.handleSubmit}>
+                    <input type="text"
+                           className={s.input}
+                           placeholder={"Name"}
+                           name={"name"}
+                           onChange={formik.handleChange}
+                           value={formik.values.name}
+                    />
+                    <input type="text"
+                           className={s.input}
+                           placeholder={"E-mail"}
+                           name={"contacts"}
+                           onChange={formik.handleChange}
+                           value={formik.values.contacts}
+                    />
+                    <textarea className={s.textarea}
+                              placeholder={"Your message"}
+                              name={"message"}
+                              onChange={formik.handleChange}
+                              value={formik.values.message}
+                    />
+                <button type={"submit"}>send</button>
                 </form>
-                <button>send</button>
             </div>
             <hr className={styleContainer.bottom_dashed}/>
         </div>
