5052c38ecd2d9e6a6f3b22a84b81f0705abdd5bc
PavelMalta <shamashov87@mail.ru>
2021-06-29 16:05:12 +0300

    added useEffect in Contact component

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useEffect} from "react";
 import s from "./Contact.module.css"
 import {Title} from "../common/components/title/Title";
 import styleContainer from "../common/styles/Container.module.css";
+import axios from "axios";
 
 
 export function Contact() {
+
+    useEffect(() => {
+        axios.post('https://smtp-nodejs-ser.herokuapp.com/sendMessage',{})
+            .then((res) => {
+                console.log('ok')
+            })
+    },[])
+
+
     return (
         <div className={s.contactsBlock}>
             <hr className={styleContainer.top_dashed}/>
